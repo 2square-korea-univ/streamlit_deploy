e8679808c3211d48194b985f49144b6fd2d91e3c
Aris <2square@korea.ac.kr>
2025-02-13 16:05:41 +0900

    Update s_test.py

--- s_test.py
+++ s_test.py
@@ -1,38 +1,24 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
 
-st.title('Uber pickups in NYC')
+st.title("st.write() 활용 예시")
 
-DATE_COLUMN = 'date/time'
-# DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-#             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-DATA_URL = 'uber-raw-data-sep14.csv'
+# 1. 텍스트 출력 (Markdown 지원)
+st.write("## 마크다운 제목")
+st.write("* 강조된 텍스트 *")
 
+# 2. 데이터프레임 출력
+df = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+st.write("### 데이터프레임")
+st.write(df)
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+# 3. 그래프 출력 (Matplotlib)
+fig, ax = plt.subplots()
+ax.plot([1, 2, 3], [1, 4, 2])
+st.write("### Matplotlib 그래프")
+st.pyplot(fig)
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
-
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+# 4. 이미지 출력
+st.write("### 이미지")
+st.image("https://streamlit.io/images/brand/streamlit-logo-horizontal.svg", width=300)
